b98f3261e2b3a45ee120a313ba81bfb4fd55358d
Goia Ciprian <goia.ciprian14@gmail.com>
2023-11-12 19:02:21 +0200

    Dev (#9)
    * disclaimer
    
    * more mobile friendly
    
    * feature/send emails (#5)
    
    * now the contact form saves submissions on firestore
    
    * now the contact form saves submissions on firestore
    
    * removed .env
    
    * some local testing
    
    * added toast notifications

--- libs/hooks/src/index.ts
+++ libs/hooks/src/index.ts
@@ -2,4 +2,4 @@ export * from './lib/useIsInViewport';
 export * from './lib/useIsBiggerThan1200';
 export * from './lib/useFirebase';
 export * from './lib/useDatabase';
-
+export * from './lib/useToast';
